43bc9290c9f9803ad1434ae0e72b643b6915ebd9
hast06001 <hast06001@E-PW024NW1>
2023-09-19 21:34:14 +0200

    Add project files.

---
+++ Uppgift 2.6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace uppgift_2._6
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv en mening med tre ord.");
+            string mening = Console.ReadLine();
+            int mellanslag1 = mening.IndexOf(" ");
+            int mellanslag2 = mening.IndexOf(" ",mellanslag1 + 1);
+            int punkt = mening.IndexOf(".");
+            string ord1 = mening[..mellanslag1];
+            string ord2 = mening[(mellanslag1 + 1)..mellanslag2];
+            string ord3 = mening[(mellanslag2 + 1)..punkt];
+            Console.WriteLine(ord3 + " " + ord1 + " " + ord2 + ".");
+
+            Console.ReadKey();
+        }
+    }
+}
